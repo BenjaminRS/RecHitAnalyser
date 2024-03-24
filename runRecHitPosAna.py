d5aa99d5d4ed249f7876eea9e50f83df7deda559
Benjamin <benjamin@cern.ch>
2024-03-24 23:03:24 +0100

    Running track refitters

--- runRecHitPosAna.py
+++ runRecHitPosAna.py
@@ -8,6 +8,13 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.Services_cff")
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load("RecoTracker.TrackProducer.TrackRefitters_cff")
+import RecoTracker.TrackProducer.TrackRefitters_cff
+process.FinalTrackRefitter = RecoTracker.TrackProducer.TrackRefitter_cfi.TrackRefitter.clone()
+process.FinalTrackRefitter.src = cms.InputTag('generalTracks','','reRECO')
+process.FinalTrackRefitter.TrajectoryInEvent = True
+process.FinalTrackRefitter.NavigationSchool = ''
+process.FinalTrackRefitter.TTRHBuilder = "WithAngleAndTemplate"
 process.GlobalTag.globaltag = "133X_dataRun3_Prompt_v2"
 
 # Load the input file containing RECO data
@@ -20,11 +27,12 @@ process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 
 # Define the analyzer module
 process.recAna = cms.EDAnalyzer('RecHitPositionAnalyzer',
-    recHitCollection = cms.InputTag('generalTracks','','reRECO')
+    # recHitCollection = cms.InputTag('generalTracks','','reRECO')
+    recHitCollection = cms.InputTag("FinalTrackRefitter")
 )
 
 process.TFileService = cms.Service("TFileService", fileName = cms.string("recHits.root") )
 
 # Path definition
-process.p = cms.Path(process.recAna)
+process.p = cms.Path(process.FinalTrackRefitter+process.recAna)
 
