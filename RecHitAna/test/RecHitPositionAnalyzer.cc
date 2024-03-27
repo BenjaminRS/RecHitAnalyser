01ce7fe9a6d8dabe7f515418f748bb57d68b9604
Benjamin <benjamin@cern.ch>
2024-03-27 10:00:02 +0100

    Performing SiStripRecHitConverter and then removing duplicates

--- RecHitAna/test/RecHitPositionAnalyzer.cc
+++ RecHitAna/test/RecHitPositionAnalyzer.cc
@@ -14,6 +14,9 @@
 #include "Geometry/CommonDetUnit/interface/GeomDet.h"
 #include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
 
+#include "DataFormats/TrackerRecHit2D/interface/SiStripMatchedRecHit2DCollection.h"
+#include "DataFormats/TrackerCommon/interface/TrackerTopology.h"
+
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 #include "TH2.h"
@@ -32,7 +35,9 @@ private:
   virtual void endJob() ;
   // InputTag for RecHit collection
   edm::EDGetTokenT<TrackingRecHitCollection> recHitCollectionToken_;
+  edm::EDGetTokenT<SiStripMatchedRecHit2DCollection> siMatchHitCollectionToken_;
   edm::ESGetToken<TrackerGeometry, TrackerDigiGeometryRecord> theGToken_;
+  edm::ESGetToken<TrackerTopology, TrackerTopologyRcd> theTopoToken_;
   TH2D *recHitsXY;
   TH2D *recHitsRZ;
 };
@@ -41,9 +46,12 @@ RecHitPositionAnalyzer::RecHitPositionAnalyzer(const edm::ParameterSet& iConfig)
   edm::Service<TFileService> fs;
   // Input tag for RecHit collection
   recHitCollectionToken_ = consumes<TrackingRecHitCollection>(iConfig.getParameter<edm::InputTag>("recHitCollection"));
+  siMatchHitCollectionToken_ = consumes<SiStripMatchedRecHit2DCollection>(iConfig.getParameter<edm::InputTag>("siMatchHitCollection"));
+  
   theGToken_ = esConsumes();
+  theTopoToken_ = esConsumes();
   recHitsXY = fs->make<TH2D>("recHitsXY", "X vs Y Position of the RecHits", 3000, -150.0, 150.0, 3000, -150.0, 150.0);
-  recHitsRZ = fs->make<TH2D>("recHitsRZ", "R vs Z Position of the RecHits", 5600, -280.0, 280.0, 1600, 0.0, 160.0);
+  recHitsRZ = fs->make<TH2D>("recHitsRZ", "R vs Z Position of the RecHits", 6000, -300.0, 300.0, 1200, 0.0, 120.0);
 }
 
 RecHitPositionAnalyzer::~RecHitPositionAnalyzer(){
@@ -59,10 +67,57 @@ void RecHitPositionAnalyzer::analyze(const edm::Event& iEvent, const edm::EventS
   // extract tracker geometry
   edm::ESHandle<TrackerGeometry> theG = iSetup.getHandle(theGToken_);
 
+  // extract tracker topology
+  edm::ESHandle<TrackerTopology> theTopo = iSetup.getHandle(theTopoToken_);
+
   // Loop over Tracking RecHits and print their positions
   for (const auto& recHit : *recHits) {
     if(recHit.isValid()){
+      DetId thedetid = recHit.rawId();
+      auto subdetId = thedetid.subdetId();
+      // auto GeomDet = theG->idToDet(recHit.geographicalId());
+      // auto SubDet = GeomDet->subDetector();
+      // std::cout << "\t\t subdetId: " << subdetId
+      //               << " SubDet: " << SubDet
+      //               << " layer: " << theTopo->layer(thedetid)
+      //               << " tidRing: " << theTopo->tidRing(thedetid)
+      //               << " tecRing: " << theTopo->tecRing(thedetid)
+      //               << " isDoubleSens: " << theTopo->isDoubleSens(thedetid)
+      //               << " tibLayer: " << theTopo->tibLayer(thedetid)
+      //               << " tibIsStereo: " << theTopo->tibIsStereo(thedetid)
+      //               << " isStereo: " << theTopo->isStereo(thedetid)
+      //               << " tibIsDoubleSide?: " << theTopo->tibIsDoubleSide(thedetid)
+      //               << " tidIsDoubleSide?: " << theTopo->tidIsDoubleSide(thedetid)
+      //               << " tobIsDoubleSide?: " << theTopo->tobIsDoubleSide(thedetid)
+      //               << " tecIsDoubleSide?: " << theTopo->tecIsDoubleSide(thedetid)
+      //               << std::endl;
+      // if(theTopo->isStereo(thedetid)==1) continue;
+
+      // The tibIsDoubleSide etc should have worked but did not. Doing it manually:
+      // subdetId 3=TIB, 4=TID, 5=TOB, 6=TEC
+      if((subdetId==3 && (theTopo->layer(thedetid)==1 || theTopo->layer(thedetid)==2)) ||
+          (subdetId==4 && (theTopo->tidRing(thedetid)==1 || theTopo->tidRing(thedetid)==2)) ||
+          (subdetId==5 && (theTopo->layer(thedetid)==1 || theTopo->layer(thedetid)==2)) ||
+          (subdetId==6 && (theTopo->tecRing(thedetid)==1 || theTopo->tecRing(thedetid)==2 || theTopo->tecRing(thedetid)==5))
+        ) continue;
+
       const auto& gPosition = theG->idToDet((recHit).geographicalId())->surface().toGlobal((recHit).localPosition());
+      recHitsXY->Fill(gPosition.x(),gPosition.y());
+      recHitsRZ->Fill(gPosition.z(),gPosition.perp());
+      // std::cout << "\t\t\tRecHit in GP x y z: " << gPosition.x() << " " << gPosition.y() << " " << gPosition.z() << std::endl;
+    }
+  }
+
+  // Get the SiStripRecHitConverter collection
+  edm::Handle<SiStripMatchedRecHit2DCollection> siMatchHits;
+  iEvent.getByToken(siMatchHitCollectionToken_, siMatchHits);
+  const SiStripMatchedRecHit2DCollection::DataContainer& stripcollStm = siMatchHits->data();
+  SiStripMatchedRecHit2DCollection::DataContainer::const_iterator istripStm;
+
+  for (istripStm = stripcollStm.begin(); istripStm != stripcollStm.end(); ++istripStm) {
+    const SiStripMatchedRecHit2D* recHit = &*(istripStm);
+    if(recHit->isValid()){
+      const auto& gPosition = theG->idToDet(recHit->geographicalId())->surface().toGlobal(recHit->localPosition());
       recHitsXY->Fill(gPosition.x(),gPosition.y());
       recHitsRZ->Fill(gPosition.z(),gPosition.perp());
       // std::cout << "\t\t\tRecHit in GP x y z: " << gPosition.x() << " " << gPosition.y() << " " << gPosition.z() << std::endl;
--- runRecHitPosAna.py
+++ runRecHitPosAna.py
@@ -8,14 +8,18 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.Services_cff")
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = "133X_dataRun3_Prompt_v2"
+process.load('RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitConverter_cfi')
+
+#Loading the Track Reffiter:
 process.load("RecoTracker.TrackProducer.TrackRefitters_cff")
 import RecoTracker.TrackProducer.TrackRefitters_cff
 process.FinalTrackRefitter = RecoTracker.TrackProducer.TrackRefitter_cfi.TrackRefitter.clone()
 process.FinalTrackRefitter.src = cms.InputTag('generalTracks','','reRECO')
 process.FinalTrackRefitter.TrajectoryInEvent = True
+# process.FinalTrackRefitter.useHitsSplitting = cms.bool(True) #This crashes due to an assert
 process.FinalTrackRefitter.NavigationSchool = ''
 process.FinalTrackRefitter.TTRHBuilder = "WithAngleAndTemplate"
-process.GlobalTag.globaltag = "133X_dataRun3_Prompt_v2"
 
 # Load the input file containing RECO data
 process.source = cms.Source("PoolSource",
@@ -25,14 +29,32 @@ process.source = cms.Source("PoolSource",
 # Set the maximum number of events to process
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 
+# Run the SiStripRecHitConverter in order to get the matched SiStrip modules and the z coordinate for those:
+process.SiStripRHConv = cms.EDProducer("SiStripRecHitConverter",    
+   ClusterProducer = cms.InputTag("siStripClusters"),    
+   rphiRecHits = cms.string("rphiRecHit"),    
+   stereoRecHits = cms.string("stereoRecHit"),    
+   matchedRecHits = cms.string("matchedRecHit"),    
+   doMatching = cms.bool(True)
+)
+
 # Define the analyzer module
 process.recAna = cms.EDAnalyzer('RecHitPositionAnalyzer',
     # recHitCollection = cms.InputTag('generalTracks','','reRECO')
-    recHitCollection = cms.InputTag("FinalTrackRefitter")
+    recHitCollection = cms.InputTag("FinalTrackRefitter"),
+    siMatchHitCollection = cms.InputTag("SiStripRHConv","matchedRecHit","recAna" )
 )
 
 process.TFileService = cms.Service("TFileService", fileName = cms.string("recHits.root") )
 
+# # In order to find the event content after running:
+# process.load('Configuration.StandardSequences.EndOfProcess_cff')
+# process.Output = cms.OutputModule("PoolOutputModule",
+#     fileName = cms.untracked.string('file:TEST.root'),
+#     splitLevel = cms.untracked.int32(0)
+# )
+# process.Out = cms.EndPath(process.Output)
+
 # Path definition
-process.p = cms.Path(process.FinalTrackRefitter+process.recAna)
+process.p = cms.Path(process.SiStripRHConv+process.FinalTrackRefitter+process.recAna)
 
